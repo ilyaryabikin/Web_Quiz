368901a10b20809128f79cd3be6ac4fbbc04a278
ilyaryabikin <ilyaryabikin0708@gmail.com>
2020-03-31 23:26:58 +0300

    Added method to handle JsonParseException

--- src/main/java/webquiz/engine/endpoint/QuizRestControllerAdvice.java
+++ src/main/java/webquiz/engine/endpoint/QuizRestControllerAdvice.java
@@ -1,5 +1,6 @@
 package webquiz.engine.endpoint;
 
+import com.fasterxml.jackson.core.JsonParseException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -17,12 +18,7 @@ public class QuizRestControllerAdvice {
     @ExceptionHandler(MethodArgumentNotValidException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public Map<String, String> handleMethodArgumentNotValidException(Exception e) {
-        Map<String, String> response = new LinkedHashMap<>();
-        response.put("timestamp", LocalDateTime.now().toString());
-        response.put("status", String.valueOf(HttpStatus.BAD_REQUEST.value()));
-        response.put("error", HttpStatus.BAD_REQUEST.getReasonPhrase());
-        response.put("message", e.getMessage());
-        return response;
+        return getBadRequestResponse(e);
     }
 
     @ExceptionHandler(NoSuchElementException.class)
@@ -32,6 +28,21 @@ public class QuizRestControllerAdvice {
         response.put("timestamp", LocalDateTime.now().toString());
         response.put("status", String.valueOf(HttpStatus.NOT_FOUND.value()));
         response.put("error", HttpStatus.NOT_FOUND.getReasonPhrase());
+        response.put("message", e.getMessage());
+        return response;
+    }
+
+    @ExceptionHandler(JsonParseException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Map<String, String> handleJsonParseException(Exception e) {
+        return getBadRequestResponse(e);
+    }
+
+    private Map<String, String> getBadRequestResponse(Exception e) {
+        Map<String, String> response = new LinkedHashMap<>();
+        response.put("timestamp", LocalDateTime.now().toString());
+        response.put("status", String.valueOf(HttpStatus.BAD_REQUEST.value()));
+        response.put("error", HttpStatus.BAD_REQUEST.getReasonPhrase());
         response.put("message", e.getMessage());
         return response;
     }
